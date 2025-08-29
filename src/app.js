21bc2e77100db97a682632fae058e769b6fbf55d
kavya sree <mail id>
2025-08-29 10:39:03 +0530

    exploring get,post,delete

--- src/app.js
+++ src/app.js
@@ -1,17 +1,21 @@
 const express=require('express');
 const app=express();
 
+
+app.get("/user",(req,res)=>{
+    res.send({firstName:"kavyasree",lastName:"Gorjila"});
+});
+
+app.post("/user",(req,res)=>{
+    console.log("data saved successfully...");
+    res.send("data added to database");
+});
+app.delete("/user",(req,res)=>{
+    res.send("deleted successfully");
+});
 app.use("/",(req,res)=>{
-    res.send("hehheee");
+    res.send("helooo...hello");
 });
-app.use("/hello",(req,res)=>{
-    res.send("helooo...node");
-});
-app.use("/test",(req,res)=>{
-    res.send("testing....");
-});
-
-
 
 
 
